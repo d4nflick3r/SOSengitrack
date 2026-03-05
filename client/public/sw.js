40d601ac17371fd52356de1ad11ea49a3d8f75d0
d4nflick3r <danielflicker91@gmail.com>
2026-03-05 23:50:24 +0000

    Fix PWA: client/public/sw.js

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sosengitrack-v2';
+const CACHE_NAME = 'sosengitrack-v3';
 const BASE = self.location.pathname.replace(/sw\.js$/, '');
 
 self.addEventListener('install', (event) => {
