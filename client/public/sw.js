323ad56c79fd3e85b9b8a84ba8c9035763d74f6a
d4nflick3r <danielflicker91@gmail.com>
2026-03-06 22:50:46 +0000

    Update sw.js

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sosengitrack-v3';
+const CACHE_NAME = 'sosengitrack-v4';
 const BASE = self.location.pathname.replace(/sw\.js$/, '');
 
 self.addEventListener('install', (event) => {
